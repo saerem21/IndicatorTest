6274114b6bd638dd808c215d1f30ae245f07d8c4
saerem21 <nemessisdds>
2014-01-03 15:46:47 +0900

    로딩창 테스트입니다..
    0.5초 딜레이가 있습니다..

--- IndicatorTest/ViewController.m
+++ IndicatorTest/ViewController.m
@@ -10,14 +10,33 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *indi1;
+
+@property (strong, nonatomic) UIActivityIndicatorView *indi2;
+
 @end
 
 @implementation ViewController
+- (IBAction)startIndi:(id)sender {
+    [self.indi1 startAnimating];
+    [self.indi2 performSelector:@selector(startAnimating) withObject:nil afterDelay:0.5];
+    
+}
+- (IBAction)stopIndi:(id)sender {
+    [self.indi1 stopAnimating];
+    [self.indi2 performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.5];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.indi2 = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.indi2.hidesWhenStopped = YES;
+    self.indi2.center = CGPointMake(160, 350);
+    [self.view addSubview:self.indi2];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
